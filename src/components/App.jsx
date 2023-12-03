dac0a7b4dbcee0449c3a95192fb7fb602eb7dae5
Kirra-Potiekhina <kirrapotehina@gmail.com>
2023-12-03 23:25:47 +0200

    add task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import user from '../assets/user.json';
+import { Profile } from './task1/Profile';
+
 export const App = () => {
   return (
     <div
@@ -10,7 +13,17 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+
+      <Profile 
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats} 
+      
+      followers={user.stats.followers}
+      views={user.stats.views}
+      likes={user.stats.likes} />
     </div>
   );
 };
